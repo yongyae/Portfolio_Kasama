b492b9fc0750937d5cf908aed8a34bd056d20dc8
yongyae <152171836+yongyae@users.noreply.github.com>
2026-05-13 12:36:02 +0700

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import Portfolio from './portfolio.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Portfolio />
+    <Analytics />
   </React.StrictMode>
 )
